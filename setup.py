38618587d8de0047ab9474782c1e532cd694afa4
Fred Drake <fdrake@acm.org>
2010-04-01 16:34:05 +0000

    set version number for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="zc.time",
     description="Hook for datetime for testing",
-    version="0",
+    version="0.2",
     license="ZVSL 1.0",
     packages=find_packages('src'),
     include_package_data=True,
