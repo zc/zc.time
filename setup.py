558c7dccbbefb764bedc03cfbc943802c70c6fa1
Fred Drake <fdrake@acm.org>
2010-04-01 16:30:52 +0000

    whitespace fix

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "zc.time",
-    description = "Hook for datetime for testing",
-    version = "0",
-    license = "ZVSL 1.0",
-    packages = find_packages('src'),
-    include_package_data = True,
-    zip_safe = False,
-    package_dir = {'':'src'},
-    namespace_packages = ['zc'],
-    install_requires = [
+    name="zc.time",
+    description="Hook for datetime for testing",
+    version="0",
+    license="ZVSL 1.0",
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    package_dir={'': 'src'},
+    namespace_packages=['zc'],
+    install_requires=[
         'pytz',
         'setuptools',
         ],
